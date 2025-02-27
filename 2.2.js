4a55079ee0d954c1137ff832bafab1a93d10d9c5
Liana <evangelia.alevrakis@gmail.com>
2025-02-27 12:33:00 +0100

    Update 2.2.js
    Needs correcting, I will come back to it later

--- 2.2.js
+++ 2.2.js
@@ -1,1 +1,10 @@
-2.2.js
+let min = prompt("Give first number?"); 
+let max = prompt("Give second number"); 
+let current = prompt("Give third number"); 
+if ((current < max) && (current > min)) { 
+  console.log(current);
+  } else if (min > max) 
+  {console.log("You suck");
+  } else {console.log("No comment");
+
+//The code isn't working, needs correcting, I will do so later 
