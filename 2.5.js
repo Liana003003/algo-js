36fde31f39f54126a3399ffe2d883c90f166af94
Liana <evangelia.alevrakis@gmail.com>
2025-02-27 15:37:27 +0100

    Update 2.5.js

--- 2.5.js
+++ 2.5.js
@@ -1,7 +1,5 @@
 let userNumber = prompt("What is your favorite number?");
-while (userNumber !== 42) { 
-  console.log("Are you sure");
+do { 
 prompt("What is your favorite number?");
-}
-
-//Code won't stop after I enter 42, need to fix later
+  console.log("Are you sure");
+} while (userNumber === 42);
