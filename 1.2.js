b1eb1a1342cbab3962c7e365212b77d0715c4f96
Liana <evangelia.alevrakis@gmail.com>
2025-02-27 11:09:06 +0100

    Update 1.2.js

--- 1.2.js
+++ 1.2.js
@@ -1,5 +1,5 @@
 let name = "Evangelia"
 let firstName = "Alevrakis"
 let city = "Namur"
-console.log('Your name is ' + name + ' ' + firstName + ' and you leave in ' + city)
-Your name is Evangelia Alevrakis and you leave in Namur
+console.log('Your name is ' + name + ' ' + firstName + ' and you live in ' + city)
+Your name is Evangelia Alevrakis and you live in Namur
