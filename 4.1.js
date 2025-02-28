3283656b2d94e30f3b5f5477cbe492afeed9fa1f
Liana <evangelia.alevrakis@gmail.com>
2025-02-28 11:29:51 +0100

    Update 4.1.js

--- 4.1.js
+++ 4.1.js
@@ -5,3 +5,5 @@ a = prompt("What is the length of the rectangle ");
 b = prompt("What is the width of the rectangle "); 
 console.log(calcSurface(a, b));
 
+/* Function calcSurface is meant to calculate the surface of a rectangle using numbers given outside the function. 
+a is substituting lenght while b is the width. The function returns the math needed to calculate the surface, length x width */
