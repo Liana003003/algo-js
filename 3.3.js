504e2a04e736121f492cac4d465fd7a7625a2c47
Liana <evangelia.alevrakis@gmail.com>
2025-02-28 10:14:21 +0100

    Update 3.3.js

--- 3.3.js
+++ 3.3.js
@@ -1,1 +1,15 @@
+//Duplicate option 1
+const clothes = ["t-shirt", "shoes", "tie", "suit"];
+const clothes2 = clothes;
+console.log(clothes2);
 
+//Duplicate option 2
+const clothes = ["t-shirt", "shoes", "tie", "suit"];
+const costume = [...clothes];
+costume.push();
+console.log(costume);  //It works but not sure if this is the one that we were meant to do
+
+//Duplicate option 3
+const clothes = ["t-shirt", "shoes", "tie", "suit"];
+const costume = clothes.slice();
+console.log(costume);
