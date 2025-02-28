e7ca7bb593b2b500fbfc504dfb3f7c65a5a43f17
Liana <evangelia.alevrakis@gmail.com>
2025-02-28 16:32:01 +0100

    Update 4.4.js

--- 4.4.js
+++ 4.4.js
@@ -1,1 +1,14 @@
+function rand10() {
+  return Math.floor((Math.random() * 10) + 1);
+}
+function pickLearner(inputAr, n) {
+if (n > 0 && n < inputAr.length) {
+return inputAr[n]; 
+} else { console.log("You are wrong");
+}
+let listNames = ["Antoine.C", "Ars_d", "Hugo P", "Kamal", "Liana", "M.Jordan", "Manu-cj", "Martin S", "Stephan M.", "Valentin Herman"];
+let lucky = rand10();
+let answer = pickLearner(listNames, lucky);
+Console.log(answer);
 
+//I need to review this code later
