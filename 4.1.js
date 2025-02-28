724c79d1b96695477e1dfd612522ea8d626dc8a9
Liana <evangelia.alevrakis@gmail.com>
2025-02-28 11:12:45 +0100

    Update 4.1.js

--- 4.1.js
+++ 4.1.js
@@ -2,5 +2,6 @@ function calcSurface(a, b) {
 return a * b;
 }
 a = prompt("What is the length of the rectangle ");
-b = prompt("What is the width of the rectangle "); console.log(calcSurface(a, b));
+b = prompt("What is the width of the rectangle "); 
+console.log(calcSurface(a, b));
 
