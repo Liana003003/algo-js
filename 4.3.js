18692fddab285da0a1a64ec2ecfebf611eef2a84
Liana <evangelia.alevrakis@gmail.com>
2025-03-02 19:16:19 +0100

    Update 4.3.js

--- 4.3.js
+++ 4.3.js
@@ -1,11 +1,12 @@
 function rand10() {
-  return Math.floor((Math.random() * 10) + 1);
-}
-function multiRand(n) { 
-for (let i = 0; i < n; i++){
-console.log(rand10());
-}
-}
-multiRand(prompt("Enter a number between 1 and 10 "));
-
-/* 
+    return Math.floor((Math.random() * 10) + 1);
+  }
+  let number_array = [];
+  function multiRand(n) { 
+  for (let i = 0; i < n; i++){ 
+  let num = rand10();
+  number_array.push(num);
+  }
+  }
+  multiRand(prompt("Enter a number between 1 and 10 "));
+  console.log(number_array);
